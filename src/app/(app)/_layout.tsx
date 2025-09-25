62d987acfb64eb7b208672217cd1ac00cd0aabf8
sarvepalli1-admin <sarat.sarvepalli@open.ac.uk>
2025-09-25 19:19:31 +0100

    initial

---
+++ src/app/(app)/_layout.tsx
@@ -0,0 +1,39 @@
+import { Tabs } from "expo-router";
+import AntDesign from "@expo/vector-icons/AntDesign";
+
+export default function AppLayout() {
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="user" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="history"
+        options={{
+          title: "History",
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="clockcircle" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
